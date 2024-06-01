65d92ab6a0e1daa15ae6d6af16cdffb31e36f321
HridaySarder <hridoyaug12@gmail.com>
2024-06-01 23:17:56 +0600

    added sort_it

---
+++ sort_it.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student
+{
+public:
+    string nm;
+    int cls;
+    string s;
+    int id;
+    int math_marks;
+    int eng_marks;
+
+    int total;
+};
+bool cmp(Student a, Student b)
+{
+    if (a.total > b.total)
+    {
+        return true;
+    }
+    else if (a.total < b.total)
+    {
+        return false;
+    }
+    else
+    {
+        if (a.id < b.id)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    Student a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].nm >> a[i].cls >> a[i].s >> a[i].id >> a[i].math_marks >> a[i].eng_marks;
+        a[i].total = a[i].math_marks + a[i].eng_marks;
+    }
+    sort(a, a + n, cmp);
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i].nm << " " << a[i].cls << " " << a[i].s << " " << a[i].id << " " << a[i].math_marks << " " << a[i].eng_marks << endl;
+    }
+    return 0;
+}
